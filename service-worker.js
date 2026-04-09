176cef97be86e49db5fcd83df1acd6e21127927c
dkeatmobile-droid <dkeatmobile@gmail.com>
2026-04-09 16:34:34 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "fatfighters-cache-v1";
+const CACHE_NAME = "fatfighters-cache-v2";
+
 const FILES_TO_CACHE = [
   "index.html",
   "manifest.json",
@@ -15,7 +16,13 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
     )
   );
   self.clients.claim();
@@ -23,6 +30,8 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
